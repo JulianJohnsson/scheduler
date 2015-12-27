7a54c030ab260e1b38b61c1ccb7a4b183bd6a44f
Julien Janson <juljanson@gmail.com>
2015-12-27 20:18:06 +0100

    add bootstrap

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,6 @@
 module EventsHelper
+
+	def image_from_amazon(image_id)
+		image_tag "http://images.amazon.com/images/P/#{image_id}.01.ZTZZZZZZ.jpg"
+	end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
+	
+scope :finished, -> { where('finish_at <= ?', DateTime.now) }
+
 	def finished?
 		finish_at < Time.now
 	end
