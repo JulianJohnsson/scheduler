67236cb3bac4bff4bd9b5e71274077a00f9ce0fb
Julien Janson <juljanson@gmail.com>
2015-12-27 20:36:17 +0100

    search on titles

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
 	
-scope :finished, -> { where('finish_at <= ?', DateTime.now) }
+	scope :finished, -> { where('finish_at <= ?', DateTime.now) } 
+	scope :search, -> (keyword) { where(title: keyword) if keyword.present? }
 
 	def finished?
 		finish_at < Time.now
