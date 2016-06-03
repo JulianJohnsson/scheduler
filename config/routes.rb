01275cc98ef293a67d6ce8c8a24a0d559b52f600
JulianJohnsson <juljanson@gmail.com>
2016-06-03 20:35:16 +0200

    chatty bot

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   mount Facebook::Messenger::Server, at: 'bot'
 
   get 'bot/webhook' => 'bot#webhook'
-  post 'bot/webhook' => 'bot#receive_message'
+  #post 'bot/webhook' => 'bot#receive_message'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
